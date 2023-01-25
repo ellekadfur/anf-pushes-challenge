3d20916f8e4ba3e50132957235dc477ae7f4baf1
Elle Kadfur <ellekadfur@gmail.com>
2023-01-25 09:34:56 -0800

    Revert "add explore card VC + getProductListJson"
    This reverts commit 52c07d5096af0ea290480fc01059f1cebdc72926.

--- ANF Code Test/ANFExploreCardTableViewController.swift
+++ ANF Code Test/ANFExploreCardTableViewController.swift
@@ -4,77 +4,35 @@
 //
 
 import UIKit
-import SDWebImage
 
 class ANFExploreCardTableViewController: UITableViewController {
-    
-    var productList = [ProductData]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        getProductListJson()
+
+    private var exploreData: [[AnyHashable: Any]]? {
+        if let filePath = Bundle.main.path(forResource: "exploreData", ofType: "json"),
+         let fileContent = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
+         let jsonDictionary = try? JSONSerialization.jsonObject(with: fileContent, options: .mutableContainers) as? [[AnyHashable: Any]] {
+            return jsonDictionary
+        }
+        return nil
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if productList.count != 0 {
-            return productList.count
-        } else {
-            return 0
-        }
-        
+        exploreData?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "exploreContentCell",
-                                                      for: indexPath) as? exploreContentCell
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ExploreContentCell", for: indexPath)
+        if let titleLabel = cell.viewWithTag(1) as? UILabel,
+           let titleText = exploreData?[indexPath.row]["title"] as? String {
+            titleLabel.text = titleText
+        }
         
-        cell?.lblTitle.text = productList[indexPath.row].title ?? ""
+        if let imageView = cell.viewWithTag(2) as? UIImageView,
+           let name = exploreData?[indexPath.row]["backgroundImage"] as? String,
+           let image = UIImage(named: name) {
+            imageView.image = image
+        }
         
-        let bgImage = productList[indexPath.row].backgroundImage ?? ""
-        
-        cell?.ivBackground.sd_setImage(with: URL(string: bgImage), completed: nil)
-        return cell ?? UITableViewCell()
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "ANFExploreCardDetailsViewController") as? ANFExploreCardDetailsViewController
-        vc?.productDetail = productList[indexPath.row]
-        self.navigationController?.pushViewController(vc!, animated: true)
-        
-    }
-}
-
-//MARK: exploreContentCell Class
-class exploreContentCell : UITableViewCell {
-    
-    @IBOutlet weak var lblTitle: UILabel!
-    @IBOutlet weak var ivBackground: UIImageView!
-}
-
-//MARK: Api Calling
-extension ANFExploreCardTableViewController {
-    
-    func getProductListJson() {
-        let Url = String(format: "https://www.abercrombie.com/anf/nativeapp/qa/codetest/codeTest_exploreData.json")
-        guard let serviceUrl = URL(string: Url) else { return }
-        var request = URLRequest(url: serviceUrl)
-        request.httpMethod = "GET"
-        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
-        let session = URLSession.shared
-        session.dataTask(with: request) { (data, response, error) in
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    let decodeJson = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
-                    self.productList = try JSONDecoder().decode([ProductData].self, from: decodeJson)
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                } catch {
-                    print(error)
-                }
-            }
-        }.resume()
+        return cell
     }
 }
