2702ed8e38195d6eb622e6d9878ec1b74de6d515
Elle Kadfur <ellekadfur@gmail.com>
2023-01-25 09:35:37 -0800

    Revert "init pods"
    This reverts commit 936bb643ea6941ef56c95187d0ffc36fd85027dc.

--- Pods/Target Support Files/Pods-ANF Code Test/Pods-ANF Code Test-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ANF_Code_Test : NSObject
-@end
-@implementation PodsDummy_Pods_ANF_Code_Test
-@end
--- Pods/Target Support Files/Pods-ANF Code TestTests/Pods-ANF Code TestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ANF_Code_TestTests : NSObject
-@end
-@implementation PodsDummy_Pods_ANF_Code_TestTests
-@end
